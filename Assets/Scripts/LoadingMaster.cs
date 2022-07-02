d19f142e7421f12fa6a2f2398dc8a6aeab5c0224
Sylux02 <79101834+Bread02@users.noreply.github.com>
2022-07-02 13:23:41 +0100

    Seen levels should now unlock properly

--- Assets/Scripts/LoadingMaster.cs
+++ Assets/Scripts/LoadingMaster.cs
@@ -10,6 +10,8 @@ public class LoadingMaster : MonoBehaviour
     [Header("Loading")]
     protected GameObject loadingCanvas;
     public Slider slider;
+
+    public GameDataManager gameDataManager;
     //   [SerializeField] private TextMeshProUGUI loadingPercentage;
 
     // loading bar
@@ -17,6 +19,7 @@ public class LoadingMaster : MonoBehaviour
 
     public virtual void Awake()
     {
+        gameDataManager = GameObject.Find("GameDataManager").GetComponent<GameDataManager>();
         loadingCanvas = GameObject.Find("LoadingCanvas");
         loadingCanvas?.SetActive(false);
         Time.timeScale = 1;
@@ -24,7 +27,72 @@ public class LoadingMaster : MonoBehaviour
 
     public void ClickNextLevel()
     {
+
+
+
         Scene scene = SceneManager.GetActiveScene();
+        switch (scene.name)
+        {
+            case "Level1":
+                gameDataManager.UnlockLevel2();
+                break;
+            case "Level2":
+                gameDataManager.UnlockLevel3();
+                break;
+            case "Level3":
+                gameDataManager.UnlockLevel4();
+                break;
+            case "Level4":
+                gameDataManager.UnlockLevel5();
+                break;
+            case "Level5":
+                gameDataManager.UnlockLevel6();
+                break;
+            case "Level6":
+                gameDataManager.UnlockLevel7();
+                break;
+            case "Level7":
+                gameDataManager.UnlockLevel8();
+                break;
+            case "Level8":
+                gameDataManager.UnlockLevel9();
+                break;
+            case "Level9":
+                gameDataManager.UnlockLevel10();
+                break;
+            case "Level10":
+                gameDataManager.UnlockLevel11();
+                break;
+            case "Level11":
+                gameDataManager.UnlockLevel12();
+                break;
+            case "Level12":
+                gameDataManager.UnlockLevel13();
+                break;
+            case "Level13":
+                gameDataManager.UnlockLevel14();
+                break;
+            case "Level14":
+                gameDataManager.UnlockLevel15();
+                break;
+            case "Level15":
+                gameDataManager.UnlockLevel16();
+                break;
+            case "Level16":
+                gameDataManager.UnlockLevel17();
+                break;
+            case "Level17":
+                gameDataManager.UnlockLevel18();
+                break;
+            case "Level18":
+                gameDataManager.UnlockLevel19();
+                break;
+            case "Level19":
+                gameDataManager.UnlockLevel20();
+                break;
+            default: break;
+        }
+
         int sceneIndex = scene.buildIndex;
         StartCoroutine(LoadAsynchronously(sceneIndex + 1));
     }
