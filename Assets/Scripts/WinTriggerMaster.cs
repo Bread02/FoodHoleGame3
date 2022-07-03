cb1f24922323089aa9818b4e10889d5716c07b61
Sylux02 <79101834+Bread02@users.noreply.github.com>
2022-07-03 12:51:03 +0100

    Created level 22

--- Assets/Scripts/WinTriggerMaster.cs
+++ Assets/Scripts/WinTriggerMaster.cs
@@ -833,8 +833,8 @@ public class WinTriggerMaster : MonoBehaviour
 
     public void CheckTimeScoreLevel22(float timer)
     {
-        float threeStarTime = 10f;
-        float twoStarTime = 20f;
+        float threeStarTime = 18f;
+        float twoStarTime = 25f;
 
         StarCheck(timer, threeStarTime, twoStarTime);
         if (levelComplete && !levelCompleteSaveProgress)
